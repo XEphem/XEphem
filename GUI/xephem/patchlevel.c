48db8e8eaaaa624f3519fbe9122e999115a70600
dave-kaye <91984396+dave-kaye@users.noreply.github.com>
2024-05-15 21:01:19 -0500

    Update release date in Help > About menu (#92)

--- GUI/xephem/patchlevel.c
+++ GUI/xephem/patchlevel.c
@@ -5,5 +5,5 @@
 #include "xephem.h"
 
 char PATCHLEVEL[] = "4.2.0";
-char PATCHDATE[] = "2021 September 13";
+char PATCHDATE[] = "2024 February 13";
 char COPYRIGHT[] = "Copyright (c) 1990-2021 by Elwood Charles Downey";
