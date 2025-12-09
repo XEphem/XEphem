fc2c224ac64a865d01f4a1ae4f1f39aa1cd4d246
ChooseAfferoGPL <coherentpositron@proton.me>
2025-12-09 01:27:38 -0600

    Update eclipse.h to stabilize total solar eclipse path calculations when GUI LAT/LON changes.
    Use a new approach for setting up Now and Obj instances for total solar eclipse path caclulations.
    
    Decouple the calculations from GUI LAT/LON changes.
    Simplify Now and Obj init and update.
    Clarify variable names.

--- libastro/eclipse.h
+++ libastro/eclipse.h
@@ -16,13 +16,18 @@ extern "C" {
 #define ASECOND (ADAY / 24.0 / 60.0 / 60.0)
 #define AYEAR (ADAY * 365.2425)
 
-void init_modifiable_instances( Now * np, Obj * po0, Obj * po1, Now * pdn, Obj *pdo0, Obj * pdo1 );
-bool calculate_decD( Obj * po0, Obj * po1, double * pdecD, double * pr0, double * pr1 );
-bool calculate_skyD( Obj * po0, Obj * po1, double decD, double * pskyD, double r0, double r1 );
-int is_eclipsing( Obj * pobj1, Obj * pobj2 );
-void increment_eclipse_time( Now * np, Obj * po0, Obj * po1, double offset );
-bool find_eclipse_start_mid_stop( Now * np, Obj * po0, Obj * po1, double * pstart, double * pmid, double * pstop );
-bool get_eclipse_path_location( Now * np, Obj * po0, Obj * po1, double * plt, double * plg );
+
+void init_eclipse_now( Now * pn, double tick );
+void init_eclipse_obj( Obj * po, PLCode c );
+void init_eclipse_now_sun_moon( Now * pn, Obj * psun, Obj * pmoon, double tick );
+void update_eclipse_now_sun_moon( Now * pn, Obj * psun, Obj * pmoon, double tick );
+bool calculate_decD( Obj * psun, Obj * pmoon, double * pdecD, double * pr0, double * pr1 );
+bool calculate_skyD( Obj * psun, Obj * pmoon, double decD, double * pskyD, double r0, double r1 );
+int is_eclipsing( Obj * psun, Obj * pmoon );
+void increment_eclipse_time( Now * pn, Obj * psun, Obj * pmoon, double offset );
+bool find_eclipse_start_mid_stop( Now * pn, Obj * psun, Obj * pmoon, double * pstart, double * pmid, double * pstop );
+bool get_eclipse_path_location( Now * pn, Obj * psun, Obj * pmoon, double * plt, double * plg );
+
 
 #ifdef __cplusplus
 }
